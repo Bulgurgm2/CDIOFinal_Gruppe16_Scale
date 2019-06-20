ba91c317c4625700ee719e6f76e8513ae1c2c47b
bulgurgm2 <s165372@student.dtu.dk>
2019-06-20 12:56:54 +0200

    Split!

--- src/main/java/AppConfig.java
+++
@@ -1,9 +0,0 @@
-import org.glassfish.jersey.server.ResourceConfig;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-//Defines api root
-@ApplicationPath("/rest")
-public class AppConfig extends ResourceConfig {
-}
